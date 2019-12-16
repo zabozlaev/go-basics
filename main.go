1e696900ab5118a7a4b82b4803a692fd8001d989
zabozlaev <snickeycs@gmail.com>
2019-12-16 16:16:53 +0300

    [add] POST request handler

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type RequestBody struct {
+	Message string `json:"msg"`
+}
+
+func requestHandler(w http.ResponseWriter, req *http.Request) {
+	method := req.Method
+
+	contentType := req.Header.Get("Content-Type")
+
+	if contentType != "application/json" {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		w.Write([]byte("Invalid content type"))
+		return
+	}
+
+	if method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("MethodNotAllowed"))
+		return
+	}
+
+	var body RequestBody
+
+	decoder := json.NewDecoder(req.Body)
+
+	if err := decoder.Decode(&body); err != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		w.Write([]byte("Invalid request body"))
+		return
+	}
+
+	fmt.Printf("Received message: %s \n", body.Message)
+
+	w.WriteHeader(http.StatusAccepted)
+
+	w.Write([]byte("Successfuly received your message!"))
+}
+
+func main() {
+	http.HandleFunc("/", requestHandler)
+
+	err := http.ListenAndServe(":3333", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
